199be8769c03a114184527beb6cbc801c072ca6a
Walid Al-Saqaf <wsaqaf@gmail.com>
2020-08-26 16:43:46 +0200

    Various improvements and updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,10 +30,13 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
 
 #  config.assets.compile = true
-  config.assets.compile = false
+#  config.assets.compile = false
+
+config.public_file_server.enabled = true
+config.assets.compile = true
 
 #  config.serve_static_assets = true
-  config.serve_static_assets = false
+#  config.serve_static_assets = false
 
   config.assets.resolve_with = [:manifest]
 
