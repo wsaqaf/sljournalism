6ca63b04cc09851ad4aa4ae0258b97b315b7885a
Walid Al-Saqaf <wsaqaf@gmail.com>
2020-08-26 16:36:57 +0200

    Various improvements and updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,9 +30,11 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
-  config.serve_static_assets = false
+#  config.serve_static_assets = false
 
-  config.assets.resolve_with = [:manifest]
+  #config.assets.resolve_with = [:manifest]
+  config.assets.resolve_with = [:manifest, :environment]
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
