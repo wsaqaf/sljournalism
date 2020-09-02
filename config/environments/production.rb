e9a2069283f1d9f281d5c4aeddb416b5684c3065
Walid Al-Saqaf <wsaqaf@gmail.com>
2020-09-02 12:43:29 +0200

    Various improvements and updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,10 +33,11 @@ Rails.application.configure do
 #  config.assets.compile = false
 
 # Disable Rails's static asset server (Apache or nginx will already do this)
-config.serve_static_assets = false
+config.serve_static_assets = true
 
 # Don't fallback to assets pipeline if a precompiled asset is missed
-config.assets.compile = false
+config.assets.compile = true
+config.assets.digest = false
 
 config.assets.quiet = true
 
